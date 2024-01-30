3c8ceb9637cf8f95794264566ce0abe097c07b3d
shashithadxdy <shashitha@360-css.com>
2024-01-30 13:03:32 +0530

    Books paginations

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -1,50 +1,59 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { BookService } from './book.service';
 import { Book } from './schemas/book.schemas';
 import { CreateBookDto } from './dto/create-book.dto';
 import { UpdateBookDto } from './dto/update-book.dto';
+import { Query as ExpressQuery } from 'express-serve-static-core';
 
 @Controller('books')
 export class BookController {
-    constructor(private bookservice: BookService) {}
+  constructor(private bookservice: BookService) {}
 
-    @Get()
-    async getAllBooks(): Promise<Book[]> {
-        return this.bookservice.findAll()
-    }
+  @Get()
+  async getAllBooks(@Query() query: ExpressQuery): Promise<Book[]> {
+    return this.bookservice.findAll(query);
+  }
 
-    @Post()
-    async createBook(
-        @Body()
-        book: CreateBookDto
-    ): Promise<Book> {
-        return this.bookservice.create(book)
-    }
+  @Post()
+  async createBook(
+    @Body()
+    book: CreateBookDto,
+  ): Promise<Book> {
+    return this.bookservice.create(book);
+  }
 
-    @Get(':id')
-    async getBook(
-        @Param('id')
-        id: string
-    ): Promise<Book> {
-        return this.bookservice.findById(id)
-    }
+  @Get(':id')
+  async getBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookservice.findById(id);
+  }
 
-    @Put(':id')
-    async updateBook(
-        @Param('id')
-        id: string,
-        @Body()
-        book: UpdateBookDto
-    ): Promise<Book> {
-        return this.bookservice.updateById(id, book)
-    }
+  @Put(':id')
+  async updateBook(
+    @Param('id')
+    id: string,
+    @Body()
+    book: UpdateBookDto,
+  ): Promise<Book> {
+    return this.bookservice.updateById(id, book);
+  }
 
-
-    @Delete(':id')
-    async deleteBook(
-        @Param('id')
-        id: string
-    ): Promise<Book> {
-        return this.bookservice.deleteById(id)
-    }
+  @Delete(':id')
+  async deleteBook(
+    @Param('id')
+    id: string,
+  ): Promise<Book> {
+    return this.bookservice.deleteById(id);
+  }
 }
